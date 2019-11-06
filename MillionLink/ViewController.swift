a09965b41d370b05087b9040fef87db10bf70179
SardarSaqibKhan <sardarsaqib294@gmail.com>
2019-11-06 12:42:10 +0500

    Initial Commit

---
+++ MillionLink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MillionLink
+//
+//  Created by Sardar Saqib on 06/11/2019.
+//  Copyright © 2019 Sardar Saqib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
